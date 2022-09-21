3d6d2771ad4f95107d552a33aa3851a4f4fbe6fc
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-09-21 13:36:41 +0100

    Added Scrolling Functionality

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,45 @@ scrollIndicators.forEach(indicator => {
         window.scrollTo(0, scrollElem.offsetTop);
     });
 });
+
+const scrollingYCoords = [
+    0, 575, 1050, 1710, 2291
+]
+
+const scrollIds = [
+    `home`, `about`, `prices`, `terms`, `contact`
+]
+
+let scrollIndex = 0;
+const scrollButton = document.getElementById(`scroll-button`);
+
+scrollButton.addEventListener(`click`, () => {
+    if(scrollIndex < scrollIds.length-1)
+        scrollIndex++;
+    
+    ScrollSections();
+})
+
+document.addEventListener(`wheel`, e => {
+    if(e.deltaY < 0 && scrollIndex > 0)
+        scrollIndex--;
+    else if (e.deltaY > 0 && scrollIndex < scrollIds.length-1)
+        scrollIndex++;
+
+    ScrollSections();
+})
+
+function ScrollSections() {
+    // Reset selected section
+    scrollIds.forEach(section => {
+        document.getElementById(section).classList.remove(`selected`);
+    })
+
+    if(scrollIndex == scrollIds.length-1)
+        scrollButton.style.display = `none`;
+    else
+        scrollButton.style.display = `flex`;
+
+    document.querySelector(`#${scrollIds[scrollIndex]}`).classList.add(`selected`);
+    window.scrollTo(0, scrollingYCoords[scrollIndex]);
+}
