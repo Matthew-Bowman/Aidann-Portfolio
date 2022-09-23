79abe2a8aa9c3b0cc5640bd035ca16949a668845
= <bowm2_21@uni.worc.ac.uk>
2022-09-23 20:21:10 +0100

    Changed table selection from works to reviews

--- src/reviews.php
+++ src/reviews.php
@@ -59,7 +59,7 @@
                 die("Connection failed: " . $conn->connect_error);
             }
 
-            $sql = "SELECT * FROM works";
+            $sql = "SELECT * FROM reviews";
             $result = $conn->query($sql);
             
             if ($result->num_rows > 0) {
