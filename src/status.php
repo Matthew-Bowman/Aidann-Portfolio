b10237a20419511b36da9591e4abae3547f1215b
= <bowm2_21@uni.worc.ac.uk>
2022-10-03 12:08:39 +0100

    Created status page

---
+++ src/status.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aidann</title>
+
+    <!-- Import Fonts -->
+    <link rel="stylesheet" type="text/css" href="./css/ECB.css">
+    <link rel="stylesheet" type="text/css" href="./css/typography.css">
+    <!-- Imported Nav & Footer Styles -->
+    <link rel="stylesheet" type="text/css" href="./css/nav.css">
+    <link rel="stylesheet" type="text/css" href="./css/footer.css">
+    <!-- Imported Global Styles -->
+    <link rel="stylesheet" type="text/css" href="./css/global.css">
+    <!-- Imported Page Specific Styles -->
+    <link rel="stylesheet" type="text/css" href="./css/status.css">
+
+    <!-- Import Google Font Icons -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,1,0" rel="stylesheet">
+
+</head>
+<body>
+    <nav>
+        <section class="nav-brand">
+            <a href="./index.php"><img src="./images/Logo.png" /></a>
+        </section>
+        <section class="nav-items">
+            <ul>
+                <li><a href="./index.php" class="paragraph active">Home</a></li>
+                <li><a href="./works.php" class="paragraph">Works</a></li>
+                <li><a href="./reviews.php" class="paragraph">Reviews</a></li>
+                <li><a href="./status.php" class="paragraph">Status</a></li>
+                <li><a href="./login.html"><img src="./images/Icons/Profile.png"/></a></li>
+            </ul>
+        </section>
+    </nav>
+
+    <?php
+    
+    // Connect to database
+    // Assign Variables
+    $servername = getenv("db_host");
+    $username = getenv("db_user");
+    $dbname = getenv("db_name");
+    $dbpass = getenv("db_pass");
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $dbpass, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM status ORDER BY position ASC;";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        // Initialise Variables
+        $sectionCounter = 0;
+        $previousType = null;
+        
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            // Assign Variables
+            $type = $row["type"];
+            $content = $row["content"];
+
+            // Write Subtitles
+            if($type == "subtitle"){
+                $sectionCounter++;
+                if($sectionCounter > 1)
+                    echo"</article></section>";
+                echo "<section id='scrollable-".$sectionCounter."'>";
+
+                echo "<article class='section-title'>";
+                echo "<span></span>";
+                echo "<h5 class='subheading'>".$row["content"]."</h5>";
+            }
+
+            if($type == "icon-google" and $previousType == "subtitle") {
+                echo "<span class='material-symbols-outlined' style='font-size: 48px'>".$content."</span>";
+            } elseif ($type == "icon-url" and $previousType == "subtitle") {
+                echo "<span><img src='".$content."' class='icon' /></span>";
+            }
+
+            if ($previousType == "subtitle")
+                echo "</article><article>";
+
+            // Write Content
+            if($type == "list-item" and $previousType != "list-item")
+                echo "<ul class='paragraph'>";
+            if($type == "list-item")
+                echo "<li>".$content."</li>";;
+            if($type != "list-item" and $previousType == "list-item")
+                echo "</ul>";
+
+            if($type == "title")
+                echo "<h2 class='title'>".$content."</h2>";
+            if($type == "heading")
+                echo "<h4 class='heading'>".$content."</h4>";
+            if($type == "inline-subheading")
+                echo "<h5 class='subheading inline'>".$content."</h5></span>";
+            if($type == "paragraph-heading")
+                echo "<p class='heading'>".$content."</p>";
+            if($type == "paragraph")
+                echo "<p class='paragraph'>".$content."</p>";
+
+            if($type == "icon-url" and $previousType != "subtitle")
+                echo "<span class='contact-handle'><img src='".$content."' class='icon' />";
+            if($type == "icon-google" and $previousType != "subtitle")
+                echo "<span class='contact-handle'><span class='material-symbols-outlined' style='font-size: 32px'>".$content."</span>";
+
+            $previousType = $row["type"];
+        }
+
+        if($previousType == "list-type")
+            echo "</ul>";
+
+        echo "</article></section>";
+    }
+    
+    $conn->close();
+    
+    ?>
+
+    <footer>
+        <h2>Aidann &#174; 2022</h2>
+        <p>All rights reserved</p>
+    </footer>
+</body>
+</html>
